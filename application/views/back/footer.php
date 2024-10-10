59188e1f9727c25d95e71f74a35f54226971b7f5
Salamul Jabbar <salamuljabbar1005@gmail.com>
2024-10-10 15:24:56 +0700

    first commit

---
+++ application/views/back/footer.php
@@ -0,0 +1,9 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+        
+    </div>
+    <strong style="text-align: center;">Copyright &copy; <script>
+            document.write(new Date().getFullYear())
+        </script> <a target="_blank" href="">SMAN 3 Lhokseumawe</a> & <a target="_blank" href="">Admin SMAN 3</a>.</strong>
+    All rights reserved.
+</footer>
